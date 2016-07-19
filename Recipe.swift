eb770a34a5bc03aa3c593108d4d94401d4dfd2aa
Aaditya (aadi) Narvekar <asnarvek@ncsu.edu>
2016-07-19 08:56:11 -0400

    Recipez - build using Core Data

---
+++ Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  Recipez
+//
+//  Created by AADITYA NARVEKAR on 7/16/16.
+//  Copyright © 2016 Aaditya Narvekar. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Recipe: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
---
+++ Recipez/RecipeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeTableViewCell.swift
+//  Recipez
+//
+//  Created by AADITYA NARVEKAR on 7/16/16.
+//  Copyright © 2016 Aaditya Narvekar. All rights reserved.
+//
+
+import UIKit
+
+class RecipeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
